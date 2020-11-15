5f8cf9fb51901cc711f713a99c1dc26f8a4beaf4
VeryGoodDev <verygooddev@outlook.com>
2020-11-14 17:30:46 -0700

    Lint fixes and stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,5 @@
-import { h } from 'preact';
+import { h } from 'preact'
 
 export default function App() {
-  return (
-    <div id="app">
-      Cool stuff soon
-    </div>
-  )
+  return <div id="app">Cool stuff soon</div>
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { h, render } from 'preact';
-import App from './components/app';
+import { h, render } from 'preact'
+import App from './components/app'
 
 render(<App />, document.querySelector(`#root`))
