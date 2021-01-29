961c92db801852073d78991ec4654e48ed8cccb1
VeryGoodDev <very_good_dev@outlook.com>
2021-01-29 03:53:11 -0700

    Created a small wrapper for <details>

---
+++ src/components/ui/Details.js
@@ -0,0 +1,29 @@
+import { h } from 'preact'
+import { useEffect, useState } from 'preact/hooks'
+import './css/details.styl'
+
+export default function Details({ defaultOpen, onToggle, summaryContent, children, ...props }) {
+  const [isOpen, setIsOpen] = useState(defaultOpen)
+  const [hasMounted, setHasMounted] = useState(false)
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+  return (
+    <details
+      className="details-component"
+      open={isOpen}
+      onToggle={() => {
+        if (!hasMounted) return
+        setIsOpen(prev => {
+          const newValue = !prev
+          if (onToggle) onToggle(newValue)
+          return newValue
+        })
+      }}
+      {...props}
+    >
+      <summary>{summaryContent}</summary>
+      {children}
+    </details>
+  )
+}
